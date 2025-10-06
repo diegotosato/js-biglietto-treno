8f61417fc590e27e96dc9ff8b58fc06414ec34c4
diegotosato <tosato1998@gmail.com>
2025-10-06 15:29:51 +0200

    create html, link js
    in file js, copy/paste the track, identification of tools and sub-tasks to be done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,44 @@
-console.log('it works');
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
+
+// TIP:
+// per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
+
+// 100km, 10 anni => prezzo corretto:  €16.80
+// 100km, 70 anni => prezzo corretto: €12.60
+
+
+// PRO TIP:
+// prima di iniziare a scrivere codice, inizia con l'elencare gli strumenti da usare per svolgere la consegna
+// e scoponi  il problema di sotto-task da svolgere. Questo ti aiuta a documentare il tuo flusso di lavoro
+// ed il codice evitandoti di restare li a fissare una pagina vuota.
+
+//tools -> const/var; operatore matematici (*, /, -); operatori relazionali su età (<18, >65); if/else per effetto degli sconti sul prezzo; proprietà toFixed(2) per decimali
+
+
+
+
+//#1 - due variabili con valore prompt, la prima mi chiede quanti chilometri voglio fare, la seconda quale è la mia età
+
+//variabile chilometri
+
+//variabile età
+
+
+
+
+
+//#2 - variabile prezzo del biglietto
+
+
+
+
+
+//#3 - if/else comparazione per gli sconti
