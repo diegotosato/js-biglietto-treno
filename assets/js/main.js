9921671c7f10c45c53bf4a169b520884d57ddd62
diegotosato <tosato1998@gmail.com>
2025-10-06 16:01:09 +0200

    add conditional instruction, adjust sales calcs, add comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,12 @@
 //#1 - due variabili con valore prompt, la prima mi chiede quanti chilometri voglio fare, la seconda quale è la mia età
 
 //variabile chilometri
-const kilo = prompt('Indicare la lunghezza della tratta in Km')
+const kilo = Number(prompt('Indicare la lunghezza della tratta in Km'))
 console.log(kilo);
 
 
 //variabile età
-const age = prompt("Indicare l'età del passeggero")
+const age = Number(prompt("Indicare l'età del passeggero"))
 console.log(age);
 
 
@@ -44,16 +44,32 @@ console.log(age);
 //#2 - variabile prezzo del biglietto
 /* the value of ticket price is an operation between travel kilometers that insert the user from kilo prompt and 0.21 euros that is the cost for single kilometer */
 const price_ticket = kilo * 0.21;
-console.log(price_ticket);
+console.log(price_ticket, 'prezzo biglietto intero');
+
 
 
 //#3 - variabili per gli sconti
 /* minorenni */
-const under_sale = (price_ticket * 20) / 100;
+const under_sale = price_ticket - ((price_ticket * 20) / 100);
+console.log(under_sale, 'prezzo biglietto under 18');
+
 
 /* over 65 */
-const over_sale = (price_ticket * 40) / 100;
+const over_sale = price_ticket - ((price_ticket * 40) / 100);
+console.log(over_sale, 'prezzo biglietto over 65');
+
 
 
 
 //# - if/else comparazione per gli sconti
+
+if (age < 18) {
+    /* se input età è minore di 18, stampo under_sale */
+    console.log(under_sale.toFixed(2), 'prezzo del biglietto da pagare');
+} else if (age > 65) {
+    /* se input età è maggiore di 65, stampo over_sale */
+    console.log(over_sale.toFixed(2), 'prezzo del biglietto da pagare');
+} else {
+    /* se input età è il restante (18<x<65), stampo price_ticket */
+    console.log(price_ticket.toFixed(2), 'prezzo del biglietto da pagare');
+}
